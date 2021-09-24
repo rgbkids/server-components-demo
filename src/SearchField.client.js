78d444f897bf74964c67335650fb691ab839a123
VTeacher rgbkids <rgbkids.g@gmail.com>
2021-09-24 17:54:37 +0900

    add: firebase

--- src/SearchField.client.js
+++ src/SearchField.client.js
@@ -14,9 +14,10 @@ import {useSignIn, useFirebase} from './fire';
 let user = null;
 
 export default function SearchField() {
-    const [text, setText] = useState('');
     const [isSearching, startSearching] = useTransition();
     const [, setLocation] = useLocation();
+
+    const [text, setText] = useState('');
     const [signed, setSigned] = useState(false);
     const [email, setEmail] = useState("");
     const [displayName, setDisplayName] = useState("");
@@ -27,44 +28,13 @@ export default function SearchField() {
             if (_user) {
                 user = _user;
 
-                console.log(user);
-
                 setSigned(true);
                 setEmail(user.email);
                 setDisplayName(user.displayName);
                 setUid(user.uid);
-
-                useFirebase().firestore().collection('study-with-me').get().then((snapshot) => {
-                    snapshot.forEach((document) => {
-                        const doc = document.data();
-                        // console.log(`---- collection('study-with-me').get() ----`);
-                        // console.log(doc);
-                    })
-                });
-
-                useFirebase().firestore().collection('study-with-me').where('email', '==', user.email).get().then((snapshot) => {
-                    snapshot.forEach((document) => {
-                        const doc = document.data();
-                        console.log(`---- collection('study-with-me').where('email', '==', user.email).get() ----`);
-                        console.log(document);
-                        console.log(document.id);
-                        console.log(doc);
-                    })
-                }).catch((error) => {
-                    console.log(error);
-                });
             }
         });
     });
-
-    async function handleAdd() {
-        if (user) {
-            const data = {
-                email: user.email,
-            }
-            useFirebase().firestore().collection('study-with-me').add(data);
-        }
-    }
 
     async function handleSignIn() {
         useSignIn();
@@ -97,7 +67,7 @@ export default function SearchField() {
             </form>
             <div>
                 {signed
-                    ? <p>{displayName}:{uid}:{email} <button onClick={() => handleAdd()}>Add</button></p>
+                    ? <p className="user-name">{displayName}</p>
                     : <button onClick={() => handleSignIn()}>Sign in</button>
                 }
             </div>
