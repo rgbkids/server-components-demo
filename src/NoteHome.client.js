6ebd266e88913d33fb70014446bc551289d19aad
VTeacher rgbkids <rgbkids.g@gmail.com>
2021-09-26 20:52:40 +0900

    fix: firebase

--- src/NoteHome.client.js
+++ src/NoteHome.client.js
@@ -5,28 +5,32 @@ import {useFirebase} from './fire';
 export default function NoteHome({notes}) {
     console.log(`NoteHome`);
 
+    const [document, setDocument] = useState();
     const [leftNotes, setLeftNotes] = useState(notes.filter((e, i) => i % 2 === 0));
     const [rightNotes, setRightNotes] = useState(notes.filter((e, i) => i % 2 === 1));
 
     useEffect(() => {
         console.log(`NoteHome useEffect`);
 
-        useFirebase().auth().onAuthStateChanged(user => {
-            console.log(`NoteHome useEffect useFirebase`);
+        if (!document) {
+            useFirebase().auth().onAuthStateChanged(user => {
+                console.log(`NoteHome useEffect useFirebase`);
 
-            if (user) {
-                useFirebase().firestore().collection('study-with-me').where('email', '==', user.email).get().then((snapshot) => {
-                    snapshot.forEach((document) => {
-                        const doc = document.data();
+                if (user) {
+                    useFirebase().firestore().collection('study-with-me').where('email', '==', user.email).get().then((snapshot) => {
+                        snapshot.forEach((document) => {
+                            const doc = document.data();
 
-                        if (doc.videos.length > 0) {
-                            setLeftNotes(doc.videos.filter((e, i) => i % 2 === 0));
-                            setRightNotes(doc.videos.filter((e, i) => i % 2 === 1));
-                        }
-                    })
-                });
-            }
-        });
+                            if (doc.videos.length > 0) {
+                                setLeftNotes(doc.videos.filter((e, i) => i % 2 === 0));
+                                setRightNotes(doc.videos.filter((e, i) => i % 2 === 1));
+                            }
+                            setDocument(document);
+                        })
+                    });
+                }
+            });
+        }
     });
 
     return (
