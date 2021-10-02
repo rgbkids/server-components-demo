464068df36ca61be13d592f7b428d6a0cc1356e9
VTeacher rgbkids <rgbkids.g@gmail.com>
2021-10-02 15:24:53 +0900

    update: key

--- src/youtube.js
+++ src/youtube.js
@@ -6,14 +6,13 @@ https://console.developers.google.com/apis/api/youtube.googleapis.com/overview?p
 // TODO: 外部に出す
 const keys = [
     "AIzaSyAEIhRMTe_KrD2jHYcZDOKoDHPKPN_nZVs",
-
-    // "AIzaSyDUEylkdRNCQxAPgi-Qkqdqsa7X3sR-nkk",
-    // "AIzaSyAkhRemmID_N_CSd2zW9GqDrPJQssTOEiY",
-    // "AIzaSyAIRDMnwqNf6zTSVhR5wQul1XBnDyH-PqI",
-    // "AIzaSyCV2LPN6RFtGFfWLtrx8OiIkhj8qD7v9Zo",
-    // "AIzaSyCC-FYd9K-VhVZVzGOiJ_ltLPwck_1bkMc",
-    // "AIzaSyAtK1DbsKVmQPl8DDpyVe_7J_CLdEdEzps",
-    // "AIzaSyCPTFcXn0V-0fEefMIAGrwUBg2o0urdU3E",
+    "AIzaSyDUEylkdRNCQxAPgi-Qkqdqsa7X3sR-nkk",
+    "AIzaSyAkhRemmID_N_CSd2zW9GqDrPJQssTOEiY",
+    "AIzaSyAIRDMnwqNf6zTSVhR5wQul1XBnDyH-PqI",
+    "AIzaSyCV2LPN6RFtGFfWLtrx8OiIkhj8qD7v9Zo",
+    "AIzaSyCC-FYd9K-VhVZVzGOiJ_ltLPwck_1bkMc",
+    "AIzaSyAtK1DbsKVmQPl8DDpyVe_7J_CLdEdEzps",
+    "AIzaSyCPTFcXn0V-0fEefMIAGrwUBg2o0urdU3E",
 ];
 
 export default function getYouTubeAPIKey() {
