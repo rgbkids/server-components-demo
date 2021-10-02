646eb8c466ea5456946483188bc72b2827d2c92e
VTeacher rgbkids <rgbkids.g@gmail.com>
2021-10-02 11:40:45 +0900

    fix: log

--- server/api.server.js
+++ server/api.server.js
@@ -8,13 +8,8 @@
 
 'use strict';
 
-// import fetch from 'node-fetch';
-// import {db} from "../src/db.server";
-
 const {fetch} = require('react-fetch');
-// const {fetch} = require('node-fetch');
 const cron = require('node-cron');
-
 
 const register = require('react-server-dom-webpack/node-register');
 register();
@@ -39,13 +34,13 @@ const ReactApp = require('../src/App.server').default;
 // Don't keep credentials in the source tree in a real app!
 const pool = new Pool(require('../credentials'));
 
-let PORT = 80;//process.env.PORT || 80;
+let PORT = 80;
 const app = express();
 
 app.use(compress());
 app.use(express.json());
 
-// http
+// HTTP
 app
     .listen(PORT, () => {
         console.log(`React Notes listening at ${PORT}...`);
@@ -70,7 +65,7 @@ app
         }
     });
 
-// https
+// HTTPS
 let server = app;
 let https = (process.env.PROTOCOL === "https:");
 if (https) {
@@ -168,26 +163,6 @@ app.get('/react', function (req, res) {
 const NOTES_PATH = path.resolve(__dirname, '../notes');
 
 const auth = async (userId, token) => {
-    // const users = await pool.query(
-    //     `select *
-    //      from users
-    //      where user_id = $1
-    //        and token = $2`,
-    //     [
-    //         "GjXKv5iQvDOdW7n2tlL6Q1Zp3fm2",
-    //         "ACzBnChwuz-x1OV9fB3XFnOejkinGVghELQVFEFjmnKuSNn_IuTkeBB_bHNR3VDemJPHeiftGCN0lvuyi1H7yf-OH5VYPq1grp53QuGpg7Ggvjx6Ajdf2xq25hQzeeHlzkNKSHgqwlE_I5k-RGlF6ZwxJR4GPdobLEI6XrZkO2olynVCoc89GuTp3O2jkp4Ot5sld8N5tztWWKz5GEDY2SHlcYoptwzpCi6KqmSQ79Un7k69Hsu2khYnqwp9xxUhc188ngJKQnUHU9mY2XK_omlp9-G3uVConFR4jA5hRVf6mrU0wnpEKjcTC2MStZyQmBZLRmAnnqToJJwkiZIyER2-yzbHcdAdo16xir6NhL4-ip99hNNC0QmIpFh864O2rF0m9SVXhJY8WkTTf5N8bfDOMIW5_TxGNs8cBuUhOdCzU51d9ABDgsq1-AwwYVPgjSURbzjGADaiUu02P0yiinP64DBhR6oVtQ"
-    //     ]
-    //
-    // ).rows;
-
-    // const users = await pool.query(
-    //     `select * from users`
-    // ).rows;
-    //
-    // console.log(`------------ auth 1 ------------`);
-    // console.log(users);
-    // console.log(`------------ auth 2 ------------`);
-
     const {rows} = await pool.query(
         `select count(*) as result
          from users
@@ -195,15 +170,8 @@ const auth = async (userId, token) => {
            and token = $2`,
         [
             userId, token
-            // "GjXKv5iQvDOdW7n2tlL6Q1Zp3fm2",
-            // "ACzBnChwuz-x1OV9fB3XFnOejkinGVghELQVFEFjmnKuSNn_IuTkeBB_bHNR3VDemJPHeiftGCN0lvuyi1H7yf-OH5VYPq1grp53QuGpg7Ggvjx6Ajdf2xq25hQzeeHlzkNKSHgqwlE_I5k-RGlF6ZwxJR4GPdobLEI6XrZkO2olynVCoc89GuTp3O2jkp4Ot5sld8N5tztWWKz5GEDY2SHlcYoptwzpCi6KqmSQ79Un7k69Hsu2khYnqwp9xxUhc188ngJKQnUHU9mY2XK_omlp9-G3uVConFR4jA5hRVf6mrU0wnpEKjcTC2MStZyQmBZLRmAnnqToJJwkiZIyER2-yzbHcdAdo16xir6NhL4-ip99hNNC0QmIpFh864O2rF0m9SVXhJY8WkTTf5N8bfDOMIW5_TxGNs8cBuUhOdCzU51d9ABDgsq1-AwwYVPgjSURbzjGADaiUu02P0yiinP64DBhR6oVtQ"
         ]
     );
-    // console.log(`------------ auth 1 ------------ userId=${userId} token=${token}`);
-    // console.log(rows);
-    // console.log(rows[0].result);
-    // console.log(`------------ auth 2 ------------`);
-
     return (rows[0].result == 1);
 }
 
@@ -211,26 +179,10 @@ const auth = async (userId, token) => {
 app.post(
     '/bookmarks',
     handleErrors(async function (req, res) {
-        // console.log(`------------ bookmarks 1 ------------ req.body.user_id=${req.body.user_id}, req.body.token=${req.body.token}`);
-
-        // TODO: 認証
         if (await auth(req.body.user_id, req.body.token) === false) {
             sendResponse(req, res, null);
-            // console.log(`------------ bookmarks 2 ------------`);
-
             return;
         }
-        // {
-        //     const {rows} = await pool.query('select * from users');
-        //     // res.json(rows);
-        //
-        //     console.log(`------------ auth 1 ------------`);
-        //     console.log(rows);
-        //     console.log(`------------ auth 2 ------------`);
-        // }
-
-        // console.log(`------------ bookmarks 3 ------------`);
-
 
         const now = new Date();
         const result = await pool.query(
@@ -239,12 +191,7 @@ app.post(
         );
 
         const insertedId = result.rows[0].bookmark_id;
-        // await writeFile(
-        //     path.resolve(NOTES_PATH, `${insertedId}.md`),
-        //     req.body.body,
-        //     'utf8'
-        // );
-        // sendResponse(req, res, insertedId);
+
         sendResponse(req, res, null);
     })
 );
@@ -255,17 +202,13 @@ app.post(
     handleErrors(async function (req, res) {
         const user_id = req.body.user_id;
         const token = req.body.token;
-
-        let returnId = "";
-
-        // const updatedId = Number(req.params.id);
         const now = new Date();
+        let returnId = "";
 
         const resultUpdate = await pool.query(
             'update users set token = $2, memo = $1, updated_at = $3 where user_id = $1 returning user_id',
             [user_id, token, now]
         );
-        console.log(`resultUpdate.rows[0].user_id=============` + resultUpdate.rows[0].user_id);
 
         if (resultUpdate.rows[0].user_id) {
             returnId = resultUpdate.rows[0].user_id;
@@ -277,13 +220,8 @@ app.post(
 
             returnId = result.rows[0].user_id;
         }
-        // await writeFile(
-        //     path.resolve(NOTES_PATH, `${insertedId}.md`),
-        //     req.body.body,
-        //     'utf8'
-        // );
+
         sendResponse(req, res, returnId);
-        // sendResponse(req, res, null);
     })
 );
 
@@ -292,7 +230,6 @@ app.put(
     handleErrors(async function (req, res) {
         const user_id = req.body.user_id;
         const token = req.body.token;
-
         const now = new Date();
         const updatedId = Number(req.params.id);
 
@@ -308,12 +245,6 @@ app.put(
             );
         }
 
-
-        // await writeFile(
-        //     path.resolve(NOTES_PATH, `${updatedId}.md`),
-        //     req.body.body,
-        //     'utf8'
-        // );
         sendResponse(req, res, null);
     })
 );
@@ -321,16 +252,13 @@ app.put(
 app.delete(
     '/users/:id',
     handleErrors(async function (req, res) {
-        // TODO: 認証
         if (await auth(req.body.user_id, req.body.token) === false) {
             sendResponse(req, res, null);
-            // console.log(`------------ bookmarks 2 ------------`);
-
             return;
         }
 
         await pool.query('delete from users where id = $1', [req.params.id]);
-        // await unlink(path.resolve(NOTES_PATH, `${req.params.id}.md`));
+
         sendResponse(req, res, null);
     })
 );
@@ -338,18 +266,13 @@ app.delete(
 app.delete(
     '/bookmarks/:id',
     handleErrors(async function (req, res) {
-        // TODO: 認証
         if (await auth(req.body.user_id, req.body.token) === false) {
             sendResponse(req, res, null);
-            // console.log(`------------ bookmarks 2 ------------`);
-
             return;
         }
 
-        // console.log(`/bookmarks/:id ${req.params.id}`);
-
         await pool.query('delete from bookmarks where bookmark_id = $1', [req.params.id]);
-        // await unlink(path.resolve(NOTES_PATH, `${req.params.id}.md`));
+
         sendResponse(req, res, null);
     })
 );
@@ -395,30 +318,6 @@ async function waitForWebpack() {
     }
 }
 
-// app.get(
-//     '/sync',
-//     handleErrors(async function (req, res) {
-//         let title = req.query.title;
-//         let body = req.query.body;
-//         let id = req.query.id;
-//         let thumbnail = req.query.thumbnail;
-//
-//         console.log(`-------------------------------------------------- sync`);
-//
-//         const now = new Date();
-//         const result = await pool.query(
-//             'insert into notes (id, title, body, created_at, updated_at, thumbnail) values ($4, $1, $2, $3, $3, $5) returning id',
-//             [title, body, now, id, thumbnail]
-//         );
-//         const returning_id = result.rows[0].id;
-//
-//         res.json({
-//             result: "true",
-//             id: `${returning_id}`,
-//         });
-//     })
-// );
-
 // TODO: 外部ファイル？
 const getKey = () => {
     const keys = [
@@ -426,7 +325,6 @@ const getKey = () => {
         // https://console.developers.google.com/apis/api/youtube.googleapis.com/overview?project=
 
         "AIzaSyDUEylkdRNCQxAPgi-Qkqdqsa7X3sR-nkk",
-
         // "AIzaSyAkhRemmID_N_CSd2zW9GqDrPJQssTOEiY",
         // "AIzaSyAIRDMnwqNf6zTSVhR5wQul1XBnDyH-PqI",
         // "AIzaSyCV2LPN6RFtGFfWLtrx8OiIkhj8qD7v9Zo",
@@ -438,13 +336,10 @@ const getKey = () => {
     return keys[Math.floor(Math.random() * keys.length)];
 }
 
-
 function getYouTubeData() {
     const search = process.env.SEARCH;
     const key = getKey();
     const endPointYouTube = `https://www.googleapis.com/youtube/v3/search?key=${key}&part=snippet&type=video&eventType=live&&maxResults=100&order=date&q=${search}`;
-
-    // console.log(`YouTube: ${endPointYouTube}`);
 
     const request = require('request');
     const options = {
@@ -453,93 +348,34 @@ function getYouTubeData() {
         url: `${endPointYouTube}`,
     }
     request(options, function (error, response, body) {
-        // console.log(body);
-
         if (body.items != undefined) {
             const items = body.items;
 
             if (items && items.length > 0) {
-                // console.log(items.map);
-
                 if (items.map) {
-                    // console.log(`------------- 1`);
-
                     items.map((item) => {
-                        // console.log(`------------- 2`);
-                        // console.log(item);
-
                         const videoId = item.id.videoId;
                         const title = item.snippet.title;
                         const channelId = item.snippet.channelId;
                         const description = item.snippet.description;
                         const thumbnail = item.snippet.thumbnails.default.url;
 
-                        const titleEncode = encodeURI(title);
-                        const descriptionEncode = encodeURI(description);
-
-                        const endPoint = `/sync/?title=${titleEncode}&body=${descriptionEncode}&id=${videoId}&thumbnail=${thumbnail}`;
-
-                        // console.log(`------------- endPoint=${endPoint}`);
-
-                        // const {rows} = pool.query(
-                        //     `select count(*) as result from notes where id = $1`,
-                        //     [
-                        //         videoId
-                        //     ]
-                        // );
-                        //
-                        // console.log(rows);
-
-                        // if (rows[0].result == 0) {
                         const now = new Date();
 
-                        const res = pool.query(
+                        pool.query(
                             'update notes set title = $1, body = $2, updated_at = $3, thumbnail = $5 where id = $4 returning id',
                             [title, description, now, videoId, thumbnail]
                         ).then((result) => {
-                            // console.log(`----------------------- then >`);
-                            // console.log(result);
-                            // console.log(result.rowCount);
-                            // console.log(`----------------------- then <`);
-
                             if (result.rowCount == 0) {
                                 pool.query(
                                     'insert into notes (id, title, body, created_at, updated_at, thumbnail) values ($4, $1, $2, $3, $3, $5) returning id',
                                     [title, description, now, videoId, thumbnail]
-                                ).then((result)=>{
-                                }).catch((e)=>{
+                                ).then(() => {
+                                }).catch(() => {
                                 });
                             }
                         });
-
-                        // console.log(`----------------------- >`);
-                        // console.log(res);
-                        // console.log(`----------------------- <`);
-
-                        // res.then((value) => {
-                        //     console.log(`----------------------- res then >`);
-                        //     console.log(value);
-                        //     console.log(`----------------------- res then <`);
-                        // });
-
-                        // if (false) {
-
-                            // pool.query(
-                            //     'insert into notes (id, title, body, created_at, updated_at, thumbnail) values ($4, $1, $2, $3, $3, $5) returning id',
-                            //     [title, description, now, videoId, thumbnail]
-                            // );
-
-                        // }
-
-                        // const returning_id = result.rows[0].id;
-
-                        // console.log(result);
-                        // fetch(result);
-                        // }
                     });
-
-                    // console.log(`------------- 3`);
-
                 }
             }
         }
@@ -549,24 +385,12 @@ function getYouTubeData() {
 app.get(
     '/cron',
     handleErrors(async function (req, res) {
-        console.log(1);
+        getYouTubeData();
+        clearYouTubeData();
 
-        // try {
-            getYouTubeData();
-
-            clearYouTubeData();
-
-            res.json({
-                result: "true",
-            });
-        // } catch (e) {
-        //     console.log(e.message);
-        //
-        //     res.json({
-        //         result: "false",
-        //         message: e.message,
-        //     });
-        // }
+        res.json({
+            result: "true",
+        });
     })
 );
 
@@ -581,18 +405,9 @@ function clearYouTubeData() {
 }
 
 cron.schedule('* * * * *', () => {
-
     handleErrors(async function () {
         getYouTubeData();
 
         clearYouTubeData();
     });
-
-        // try {
-        // getYouTubeData();
-        //
-        // clearYouTubeData();
-    // } catch (e) {
-    //     console.log(e.message);
-    // }
 });
